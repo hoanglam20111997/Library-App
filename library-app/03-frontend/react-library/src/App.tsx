4311cc8dddcfd92680d6769381e3cf4481c07eb8
John Doe <john.doe@contoso.com>
2023-06-10 00:14:44 +0700

    commit for video 84

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -3,13 +3,27 @@ import './App.css';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div className='App'>
-      <Navbar/>
-      <HomePage/>
-      <Footer/>
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
