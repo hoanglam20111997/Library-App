985ccc76d9b4f6ed1cfee77af154aa51a20c1ad8
John Doe <john.doe@contoso.com>
2023-06-12 00:40:30 +0700

    commit for video 130

---
+++ library-app/03-frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oa9x21n7tzRf5TOq5d7',
+    issuer: 'https://dev-89407225.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
