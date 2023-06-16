6281abf96346b146c322f0eec0b79e0d2ab37d53
John Doe <john.doe@contoso.com>
2023-06-17 00:14:52 +0700

    commit for video 231

--- library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Message.java
+++ library-app/02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Message.java
@@ -1,14 +1,16 @@
 package com.luv2code.springbootlibrary.entity;
 
+
 import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name = "message")
+@Table(name = "messages")
 @Data
 public class Message {
-    public Message() {}
+
+    public Message(){}
 
     public Message(String title, String question) {
         this.title = title;
@@ -17,24 +19,37 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name="id")
     private Long id;
 
-    @Column(name = "user_email")
+    @Column(name="user_email")
     private String userEmail;
 
-    @Column(name = "title")
+    @Column(name="title")
     private String title;
 
-    @Column(name = "question")
+    @Column(name="question")
     private String question;
 
-    @Column(name = "admin_email")
+    @Column(name="admin_email")
     private String adminEmail;
 
-    @Column(name = "response")
+    @Column(name="response")
     private String response;
 
-    @Column(name = "closed")
+    @Column(name="closed")
     private boolean closed;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
